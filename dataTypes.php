51127b563fc2bc58e52dfb63472309cd23fe2ee0
mperorjohn <mperorjohnoyekola@gmail.com>
2025-05-21 15:58:28 +0100

    our first push

---
+++ dataTypes.php
@@ -0,0 +1,35 @@
+<?php
+
+// Data types in php
+// primitive Data types
+//          string : "john"
+//          integer : 1
+//          float : 1.5
+//          boolean : true
+//          array : ['john', 'lumanze', 'peter']
+
+
+
+$my_name = "John";          // string
+$my_age = 15;                // integer
+$my_height = 1.5;           // float
+$is_student = true;         // boolean
+$my_students = ['John', 'Lumanze', 'Peter','david','blessing','raphael','lanre']; // array
+
+
+
+echo $my_name.""."is ".$my_age."years old<br>".$my_height."is my height<br>".($is_student ?"is a " : "not a  ")."Student<br>";
+
+// Non-primitive data types
+// object
+// callable
+// resource
+// null
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+// variables
+// how variable works 
+// how to print variable on our browser
+
+
+$a = 10;
+
+// echo $a."<br>";
+
+// $b = 20;
+
+// echo $b."<br>";
+
+$isLoggedIn = false;
+
+
+$sum  = $a + $b;
+
+// echo($sum);
+// echo "<br>";
+$first_person = "John";
+// $second_person = "Peter";
+// $third_person = "David";
+// $fourth_person = "Lumanze";
+
+// echo "How are you". ' '.$first_person."<br>";
+
+// echo "How are you". ' '.$second_person."<br>";
+
+// echo "How are you". ' '.$third_person."<br>";
+
+// echo "How are you". ' '.$fourth_person."<br>";
+
+
+// echo $first_person.','.' '.$second_person.','.' '.$third_person.','.' '.$fourth_person."Are php student under Pora";
+
+
+
+// echo $first_person."<br>";
+// echo $second_person."<br>";
+// echo $third_person."<br>";
+// echo $fourth_person."<br>";
+
+
+// print_r($first_person);
+// echo "<br>";
+// print_r($second_person);
+// echo "<br>";
+// print_r($third_person);
+// echo "<br>";
+// print_r($fourth_person);
+// echo "<br>";
+
+// print($first_person);
+// echo "<br>";
+// print($second_person);
+// echo "<br>";
+// print($third_person);
+// echo "<br>";
+// print($fourth_person);
+// echo "<br>";
+
+
+// var_dump($first_person);
+// echo "<br>";
+// var_dump($second_person);
+// echo "<br>";
+// var_dump($third_person);
+// echo "<br>";
+// var_dump($fourth_person);
+// echo "<br>";
+
+// die($first_person);
+// die($second_person);
+// die($third_person);
+// die($fourth_person);
+// die($a);
+
+
+// print_r($all_of_us);
+// echo "<br>";
+
+?>
